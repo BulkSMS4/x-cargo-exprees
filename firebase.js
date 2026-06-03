66e993f7c55dc9f7433fb1881a56569e6dfd863a
BulkSMS4 <xportbox@gmail.com>
2026-06-03 04:16:07 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLYDpP68NlfYGE1O9DafgrmFBUlgpoYeI",
-  authDomain: "x-cargo-exprees.firebaseapp.com",
-  databaseURL: "https://x-cargo-exprees-default-rtdb.firebaseio.com",
-  projectId: "x-cargo-exprees",
-  storageBucket: "x-cargo-exprees.firebasestorage.app",
-  messagingSenderId: "1082201867958",
-  appId: "1:1082201867958:web:d6600fbc82085b0b62c817",
-  measurementId: "G-C3Y4PQC86V"
+  apiKey: "AIzaSyBDAsdkGgC_80d2Wzh6DCxi42BhDRmNyVM",
+  authDomain: "x-cargo-express-delivery.firebaseapp.com",
+  projectId: "x-cargo-express-delivery",
+  storageBucket: "x-cargo-express-delivery.firebasestorage.app",
+  messagingSenderId: "422452155765",
+  appId: "1:422452155765:web:d94533f838cfef656baf3c",
+  measurementId: "G-5893LBFSFK"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
